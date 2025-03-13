0c4281b902a75baf2a29c1fe845da56ed5ebe1e7
Josef Hak <jhak@mirantis.com>
2025-03-13 14:10:21 +0100

    Remove aws-credential advanced feature usage "secrets"
    - to minimize testing, used only in external-dns example

--- scripts/setup_provider_credential.sh
+++ scripts/setup_provider_credential.sh
@@ -4,8 +4,7 @@ set -euo pipefail
 if [[ "$TEST_MODE" == aws ]]; then
     helm upgrade --install aws-credential oci://ghcr.io/k0rdent/catalog/charts/aws-credential \
         --version 0.0.1 \
-        -n kcm-system \
-        -f providers/values-aws-credential.yaml
+        -n kcm-system
 
     kubectl patch secret aws-credential-secret -n kcm-system -p='{"stringData":{"AccessKeyID":"'$AWS_ACCESS_KEY_ID'"}}'
     kubectl patch secret aws-credential-secret -n kcm-system -p='{"stringData":{"SecretAccessKey":"'$AWS_SECRET_ACCESS_KEY'"}}'
