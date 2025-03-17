198f708569e381c0813e70d0f66ad48ad58eec2f
Josef Hak <jhak@mirantis.com>
2025-03-17 15:01:21 +0100

    Add wait_for_servicetemplate.sh script

---
+++ scripts/wait_for_servicetemplate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euo pipefail
+
+while true; do
+    output=$(KUBECONFIG="kcfg_k0rdent" kubectl get servicetemplate -A --no-headers)
+    echo "$output"
+    if grep -q -v "true" <<< "$output"; then
+        echo "Some service templates not validated!"
+        sleep 3
+        continue
+    fi
+    echo "All servicetemplates OK"
+    break
+done
