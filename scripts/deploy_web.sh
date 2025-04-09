1576e9c3bed3c38abd95e07313c1cda6df72fd2a
Josef Hak <jhak@mirantis.com>
2025-04-09 21:17:18 +0200

    Fix custom domain setting adding CNAME to gh-pages branch
    - k0rdent/catalog#291

--- scripts/deploy_web.sh
+++ scripts/deploy_web.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -eo pipefail
 
+# Ensure "site_url" field in mkdocs.yml (stay on the same page on version switch)
 if [[ -n "$SITE_URL" ]]; then
     echo "site url found: $SITE_URL"
     echo "site_url: $SITE_URL" >> mkdocs.yml
@@ -8,6 +9,8 @@ if [[ -n "$SITE_URL" ]]; then
 else
     echo "SITE_URL not found"
 fi
+
+# Build gh pages with a different versions support
 rm -rf mkdocs/apps
 VERSION="v0.1.0" mkdocs build # generate md files
 VERSION="v0.1.0" mike deploy v0.1.0
@@ -15,4 +18,11 @@ rm -rf mkdocs/apps
 VERSION="v0.2.0" mkdocs build # generate md files
 VERSION="v0.2.0" mike deploy v0.2.0 latest stable --update-aliases
 mike set-default v0.2.0
+
+# Ensure CNAME file in gh-pages to set custom domain
+git stash
+git checkout gh-pages
+echo "catalog.k0rdent.io" > CNAME
+git add CNAME
+git commit -m "Add CNAME to set custom domain"
 git push origin gh-pages -f
